02abb83d54cfeacc81c42aaf1ebfb6c05f7ce371
yunusemredilber <yunusemredilber@yandex.com>
2021-09-18 14:47:45 +0300

    Integrate React 18

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
