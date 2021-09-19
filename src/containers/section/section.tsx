46db522879661e0ef9cff88de4587848c483a60d
yunusemredilber <yunusemredilber@yandex.com>
2021-09-20 00:32:40 +0300

    Add section component

---
+++ src/containers/section/section.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from "react"
+import styles from "./section.module.css"
+import { Typography } from "../../components/typography"
+
+interface Props {
+  header: string
+  secondHeader: string
+}
+
+const Section: FunctionComponent<Props> = ({
+  children,
+  header,
+  secondHeader,
+}) => {
+  return (
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <Typography variant="h1">{header}</Typography>
+        <Typography variant="h2">{secondHeader}</Typography>
+      </header>
+      {children}
+    </section>
+  )
+}
+
+Section.displayName = "Section"
+
+export { Section }
